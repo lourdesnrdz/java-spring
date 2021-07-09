b09924852a719fc22635ad5e7d11ea109da6a108
lourdesnrdz <lourdesnrdz@gmail.com>
2021-07-09 14:03:21 -0500

    Purchase domain

---
+++ platzi_market-clase-12-conectar-db/platzi_market-clase-12-conectar-db/src/main/java/com/platzi/market/domain/Purchase.java
@@ -0,0 +1,72 @@
+package com.platzi.market.domain;
+
+import com.platzi.market.domain.service.PurchaseItem;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Purchase {
+    private int purchase;
+    private String clientId;
+    private LocalDateTime date;
+    private String paymentMethod;
+    private String comment;
+    private String state;
+    private List<PurchaseItem> item;
+
+    public int getPurchase() {
+        return purchase;
+    }
+
+    public void setPurchase(int purchase) {
+        this.purchase = purchase;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public List<PurchaseItem> getItem() {
+        return item;
+    }
+
+    public void setItem(List<PurchaseItem> item) {
+        this.item = item;
+    }
+}
